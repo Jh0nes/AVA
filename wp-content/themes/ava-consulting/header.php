4d8ef029af1a85a6be8714887efe436bcc87f36f
Jose Vitor <jrodrigues@infusion.com.br>
2020-01-27 19:38:15 +0000

    added first header setyles

--- wp-content/themes/ava-consulting/header.php
+++ wp-content/themes/ava-consulting/header.php
@@ -25,32 +25,44 @@
 	<a class="sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'ava-consulting' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
+		<div class="language-bar container-fluid">
+			<div class="row justify-content-end bg-secondary">
+				<div class="col-1">
+					<a href="/blog">Blog</a>
+				</div>
+				<div class="col-2">
+					Idioma: Portugues
+				</div>
+			</div>
+		</div>
+		<div class="site-branding container">
 			<?php
 			the_custom_logo();
 			if ( is_front_page() && is_home() ) :
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<!--h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1-->
 				<?php
 			else :
 				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<!--p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p-->
 				<?php
 			endif;
-			$ava_consulting_description = get_bloginfo( 'description', 'display' );
+			// $ava_consulting_description = get_bloginfo( 'description', 'display' );
 			if ( $ava_consulting_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $ava_consulting_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
+			<?php 
+				endif; 
+			?>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ava-consulting' ); ?></button>
+			<!--button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ava-consulting' ); ?></button-->
 			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
+			// wp_nav_menu( array(
+			// 	'theme_location' => 'menu-1',
+			// 	'menu_id'        => 'primary-menu',
+			// ) );
 			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
